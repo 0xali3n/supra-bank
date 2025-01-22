37bc0513ca19c74959dc1e1800d669a84a2b55e8
MrAnonymous09 <chinmayofficial2002@gmail.com>
2025-01-22 17:44:05 +0530

    Errors Solved

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Card, CardContent } from "../components/ui/Card";
 import { Input } from "../components/ui/Input";
 import { Button } from "../components/ui/Button";
